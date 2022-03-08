fd1e491e1add64526f0511199ecd388bbdff1d76
Arita Osmani <aritaaosmani@gmail.com>
2022-03-08 16:02:50 +0100

    Added single user endpoint

--- index.ts
+++ index.ts
@@ -19,6 +19,82 @@ app.get('/hobbies', async (req, res) => {
     res.send(hobbies)
 })
 
+app.post('/users', async (req, res) => {
+    const { email, name, photo, hobbies = [] } = req.body
+    const newUser = await prisma.user.create({
+        data: {
+            email: email,
+            name: name,
+            photo: photo,
+            hobbies: {
+                connectOrCreate: hobbies.map((hobby: any) => ({
+                    where: { name: hobby.name },
+                    create: hobby
+                }))
+            }
+        },
+        include: { hobbies: true }
+    })
+
+    res.send(newUser)
+})
+
+app.post('/addHobby', async (req, res) => {
+    const { email, hobbies } = req.body
+    const user = await prisma.user.update({
+        where: { email: email },
+        data: {
+            hobbies: {
+                connectOrCreate: hobbies.map((hobby: any) => ({
+                    where: {
+                        name: hobby.name
+                    },
+                    create: hobby
+                }))
+            }
+        },
+        include: { hobbies: true }
+    })
+    res.send(user)
+})
+
+app.get('/hobbies/:id', async (req, res) => {
+    const id = Number(req.params.id);
+
+    const match = await prisma.hobby.findFirst({
+        where: { id: id },
+        include: { user: { select: { name: true } } }
+    })
+    if (match) {
+
+        res.send(match)
+    } else {
+        res.status(404).send({ error: 'Hobby not found!' })
+    }
+})
+
+app.get('/users/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    const match = await prisma.user.findFirst({
+        where: {
+            id: id
+        },
+        include: {
+            hobbies: {
+                select: {
+                    name: true,
+                    active: true
+                }
+            }
+        }
+    })
+    if (match) {
+        res.status(200).send(match)
+    } else {
+        res.status(404).send({ error: 'User not found!!' })
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 })
